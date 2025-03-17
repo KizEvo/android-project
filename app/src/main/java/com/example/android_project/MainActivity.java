c94f11f9f8ebb4e4275a4f529a198fc5b4f41453
Duc Phu <vandungqc03@gmail.com>
2025-03-17 21:24:40 +0700

    app/src: update GUI to accept user input and display result
    The user can interact with the app by providing the input for
    the quadratic equation. The logic do not accept missing input.

--- app/src/main/java/com/example/android_project/MainActivity.java
+++ app/src/main/java/com/example/android_project/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.android_project;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,6 +32,22 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                Bundle bundle = new Bundle();
+
+                EditText inputA = (EditText) findViewById(R.id.editTextNumberSigned1);
+                EditText inputB = (EditText) findViewById(R.id.editTextNumberSigned2);
+                EditText inputC = (EditText) findViewById(R.id.editTextNumberSigned3);
+                try {
+                    float inputANumb = Float.parseFloat(inputA.getText().toString());
+                    float inputBNumb = Float.parseFloat(inputB.getText().toString());
+                    float inputCNumb = Float.parseFloat(inputC.getText().toString());
+                    // Store the inputs to bundle
+                    bundle.putFloat("inputA", inputANumb); bundle.putFloat("inputB", inputBNumb); bundle.putFloat("inputC", inputCNumb);
+                    intent.putExtras(bundle);
+                } catch (Exception e) {
+                    Log.d("MyApp", "User provided missing inputs");
+                }
+
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/android_project/ResultActivity.java
+++ app/src/main/java/com/example/android_project/ResultActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,7 +24,39 @@ public class ResultActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
+        // Get pointer to Result id
+        TextView result = (TextView) findViewById(R.id.res_text1);
+        // Get the inputs from MainActivity bundle
+        Bundle bundle = getIntent().getExtras();
+        // Calculate the equation
+        if (bundle != null) {
+            QuadraticEquation fx = new QuadraticEquation(
+                    bundle.getFloat("inputA"),
+                    bundle.getFloat("inputB"),
+                    bundle.getFloat("inputC")
+            );
+            String [] res = fx.Solve();
+            String output = "";
+            switch (res[0]) {
+                case "LESS":
+                    output += "Equation has 2 distinct roots";
+                    break;
+                case "EQUAL":
+                    output += "Equation has 1 distinct root";
+                    break;
+                case "LARGER":
+                    output += "Equation has no real roots";
+                    break;
+            }
+            output += "\nx0: " + res[1] + "\n";
+            output += "x1: " + res[2];
+            // Display the result
+            result.setText(output);
+        } else {
+            // Error case
+            String output = "Inputs cannot be empty";
+            result.setText(output);
+        }
         Button btn = (Button) findViewById(R.id.res_button);
 
         btn.setOnClickListener(new View.OnClickListener() {
