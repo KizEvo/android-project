4a0e6ba61e31bd5445c04a7876718b5bda2ead6f
Duc Phu <vandungqc03@gmail.com>
2025-03-31 23:32:16 +0700

    src/main/java: add logic to read from main and display to sub
    User provide infos in the main activity which then will be
    display in the sub activity.

--- app/src/main/java/com/example/android_project/MainActivity.java
+++ app/src/main/java/com/example/android_project/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.android_project;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,7 +35,61 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, SubActivity.class);
-                startActivity(intent);
+                Bundle bundle = new Bundle();
+
+                EditText nameInput = (EditText) findViewById(R.id.editTextText1);
+                EditText idInput = (EditText) findViewById(R.id.editTextText2);
+                EditText classInput = (EditText) findViewById(R.id.editTextText3);
+                EditText phoneInput = (EditText) findViewById(R.id.editTextNumberDecimal);
+
+                CheckBox year4Box = (CheckBox) findViewById(R.id.checkBox4);
+                CheckBox year3Box = (CheckBox) findViewById(R.id.checkBox2);
+                CheckBox year2Box = (CheckBox) findViewById(R.id.checkBox3);
+                CheckBox year1Box = (CheckBox) findViewById(R.id.checkBox);
+
+                CheckBox mthtnBox = (CheckBox) findViewById(R.id.checkBox19);
+                CheckBox dtBox = (CheckBox) findViewById(R.id.checkBox21);
+                CheckBox vtBox = (CheckBox) findViewById(R.id.checkBox18);
+                CheckBox naBox = (CheckBox) findViewById(R.id.checkBox20);
+
+                EditText planInput = (EditText) findViewById(R.id.editTextTextMultiLine2);
+
+                try {
+                    bundle.putString("nameInput", nameInput.getText().toString());
+                    bundle.putString("idInput", idInput.getText().toString());
+                    bundle.putString("classInput", classInput.getText().toString());
+                    bundle.putString("phoneInput", phoneInput.getText().toString());
+
+                    int year = 0;
+                    if (year4Box.isChecked()) {
+                        year = 4;
+                    } else if (year3Box.isChecked()) {
+                        year = 3;
+                    } else if (year2Box.isChecked()) {
+                        year = 2;
+                    } else if (year1Box.isChecked()) {
+                        year = 1;
+                    }
+                    bundle.putInt("yearBox", year);
+
+                    String specialized = "";
+                    if (mthtnBox.isChecked()) {
+                        specialized += "MT-HTN";
+                    } else if (dtBox.isChecked()) {
+                        specialized += "DT";
+                    } else if (vtBox.isChecked()) {
+                        specialized += "DT";
+                    } else if (naBox.isChecked()) {
+                        specialized += "N/A";
+                    }
+                    bundle.putString("specializedInput", specialized);
+                    bundle.putString("planInput", planInput.getText().toString());
+
+                    intent.putExtras(bundle);
+                    startActivity(intent);
+                } catch (Exception e) {
+                    Log.d("MyApp", Objects.requireNonNull(e.getMessage()));
+                }
             }
         });
     }
--- app/src/main/java/com/example/android_project/SubActivity.java
+++ app/src/main/java/com/example/android_project/SubActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -23,6 +24,21 @@ public class SubActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        // Get the inputs from MainActivity bundle
+        Bundle bundle = getIntent().getExtras();
+
+        TextView result = (TextView) findViewById(R.id.subTextView2);
+        String output = "";
+        if (bundle != null) {
+            output += "\nYour name is " + bundle.getString("nameInput");
+            output += "\nwith student id " + bundle.getString("idInput");
+            output += "\nyou're in your " + bundle.getInt("yearBox") + " year";
+            output += "\nyour class is " + bundle.getString("classInput");
+            output += "\nyou're specialized in " + bundle.getString("specializedInput");
+            output += "\nyour phone is " + bundle.getString("phoneInput");
+            output += "\nand you plan to\n\t" + bundle.getString("planInput");
+        }
+        result.setText(output);
         Button btn = (Button) findViewById(R.id.button2);
 
         btn.setOnClickListener(new View.OnClickListener() {
