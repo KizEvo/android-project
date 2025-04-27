715c185c682f4b087e4a36cc29d45554b5a8e470
LongLe7184 <lengoclong2003@gmail.com>
2025-04-27 17:32:47 +0700

    April-27th Backup - Added Horizontal Scrollview for banners - Added a sub activity for banner click

---
+++ app/src/main/java/com/example/android_project/appUI/BannerClickActivity.java
@@ -0,0 +1,55 @@
+package com.example.android_project.appUI;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.android_project.R;
+
+public class BannerClickActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_banner_click);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bannerClickSV), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        String choseBanner = getIntent().getStringExtra("choseBanner");
+        if(choseBanner != null){
+            if(choseBanner.equals("banner1")){
+
+            } else if (choseBanner.equals("banner2")) {
+
+            } else if (choseBanner.equals("banner3")) {
+
+            } else if (choseBanner.equals("banner4")) {
+
+            } else if (choseBanner.equals("banner5")) {
+
+            }
+        }
+
+        //Back to HomePage
+        Button backBT = findViewById(R.id.backBT);
+        backBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+//                Intent intent = new Intent(BannerClickActivity.this, HomeFragment.class);
+//                startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/android_project/appUI/HomeFragment.java
+++ app/src/main/java/com/example/android_project/appUI/HomeFragment.java
@@ -1,12 +1,17 @@
 package com.example.android_project.appUI;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
 
 import com.example.android_project.R;
 
@@ -23,10 +28,86 @@ public class HomeFragment extends Fragment {
 //        }
 //    }
 
+    int scrollPosition = 0;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        /*-----AUTO-SCROLL BEHAVIOR FOR SCROLLVIEW BANNERS-----*/
+        Handler handler = new Handler();
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                HorizontalScrollView scrollView = view.findViewById(R.id.horizontalScrollView);
+                scrollPosition += 380;
+                Log.d("DEBUG", "scrollPos = " + scrollPosition + "Boundary = " + scrollView.getChildAt(0).getWidth());
+                if (scrollPosition >= 380*3) {
+                    scrollPosition = 0; // Reset to the beginning
+                }
+                scrollView.smoothScrollTo(scrollPosition, 0);
+                handler.postDelayed(this, 3000); // Change delay as needed
+            }
+        };
+
+        // Start auto-scrolling
+        handler.postDelayed(runnable, 3000);
+
+        /*-----CLICKABLE IMAGEVIEW BEHAVIOR-----*/
+        ImageView banner1 = view.findViewById(R.id.banner1);
+        ImageView banner2 = view.findViewById(R.id.banner2);
+        ImageView banner3 = view.findViewById(R.id.banner3);
+        ImageView banner4 = view.findViewById(R.id.banner4);
+        ImageView banner5 = view.findViewById(R.id.banner5);
+
+        banner1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), BannerClickActivity.class);
+                intent.putExtra("choseBanner", "banner1");
+                startActivity(intent);
+            }
+        });
+
+        banner2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), BannerClickActivity.class);
+                intent.putExtra("choseBanner", "banner2");
+                startActivity(intent);
+            }
+        });
+
+        banner3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), BannerClickActivity.class);
+                intent.putExtra("choseBanner", "banner3");
+                startActivity(intent);
+            }
+        });
+
+        banner4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), BannerClickActivity.class);
+                intent.putExtra("choseBanner", "banner4");
+                startActivity(intent);
+            }
+        });
+
+        banner5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), BannerClickActivity.class);
+                intent.putExtra("choseBanner", "banner5");
+                startActivity(intent);
+            }
+        });
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
     }
 }
