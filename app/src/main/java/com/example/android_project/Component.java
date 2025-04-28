e4d3215ada54c09c9ee769a3e37c0242ee64e417
Duc Phu <vandungqc03@gmail.com>
2025-04-28 23:35:16 +0700

    app/src/main: add exercise 5.2

--- app/src/main/java/com/example/android_project/Component.java
+++ app/src/main/java/com/example/android_project/Component.java
@@ -16,11 +16,14 @@ public class Component extends BaseAdapter {
 
     private int[] resId;
 
-    public Component(Activity activity, String[] text, String[] textVersion, int[] resId) {
+    private int tab;
+
+    public Component(Activity activity, String[] text, String[] textVersion, int[] resId, int tab) {
         this.text = text;
         this.activity = activity;
         this.textVersion = textVersion;
         this.resId = resId;
+        this.tab = tab;
     }
 
     @Override
@@ -42,16 +45,29 @@ public class Component extends BaseAdapter {
     public View getView(int i, View view, ViewGroup viewGroup) {
         LayoutInflater inflater = activity.getLayoutInflater();
 
-        view = inflater.inflate(R.layout.item, null);
+        if (this.tab == 0) {
+            view = inflater.inflate(R.layout.item, null);
 
-        TextView textTV = (TextView) view.findViewById(R.id.text);
-        textTV.setText(this.text[i]);
+            TextView textTV = (TextView) view.findViewById(R.id.text);
+            textTV.setText(this.text[i]);
 
-        TextView textVersionTV = (TextView) view.findViewById(R.id.textVersion);
-        textVersionTV.setText(this.textVersion[i]);
+            TextView textVersionTV = (TextView) view.findViewById(R.id.textVersion);
+            textVersionTV.setText(this.textVersion[i]);
 
-        ImageView imageIV = (ImageView) view.findViewById(R.id.image);
-        imageIV.setImageResource(this.resId[i]);
+            ImageView imageIV = (ImageView) view.findViewById(R.id.image);
+            imageIV.setImageResource(this.resId[i]);
+        } else {
+            view = inflater.inflate(R.layout.fruit, null);
+
+            TextView textTV = (TextView) view.findViewById(R.id.text);
+            textTV.setText(this.text[i]);
+
+            TextView textVersionTV = (TextView) view.findViewById(R.id.textVersion);
+            textVersionTV.setText(this.textVersion[i]);
+
+            ImageView imageIV = (ImageView) view.findViewById(R.id.image);
+            imageIV.setImageResource(this.resId[i]);
+        }
 
         return view;
     }
--- app/src/main/java/com/example/android_project/MainActivity.java
+++ app/src/main/java/com/example/android_project/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.android_project;
 
+import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
@@ -22,12 +27,27 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        Activity activity = this;
         String[] text = {"Android Cupcakes", "Android Flamingo", "Android Small", "Android Big", "Android Medium", "Android Cockatiel", "Android Lovebird"};
         String[] textVersion = {"Version 1.2", "Version 1.3", "Version 1.4", "Version 1.5", "Version 1.6", "Version 1.7", "Version 1.8"};
         int[] resId = {R.drawable.android, R.drawable.android1, R.drawable.android2, R.drawable.android1, R.drawable.android, R.drawable.android2, R.drawable.android1};
 
         listView = (ListView) findViewById(R.id.list);
-        Component adapter = new Component(this, text, textVersion, resId);
+        Component adapter = new Component(this, text, textVersion, resId, 0);
         listView.setAdapter(adapter);
+
+        Button btn = (Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] newText = {"Orange", "Cherry", "Banana", "Apple"};
+                String[] newTextVersion = {"47 Calories", "50 Calories", "89 Calories", "52 Calories"};
+                int[] newResId = {R.drawable.orange, R.drawable.cherry, R.drawable.banana, R.drawable.apple};
+                LinearLayout viewLL = (LinearLayout) findViewById(R.id.main);
+                viewLL.setBackgroundColor(Color.DKGRAY);
+                Component newAdapter = new Component(activity, newText, newTextVersion, newResId, 1);
+                listView.setAdapter(newAdapter);
+            }
+        });
     }
 }
