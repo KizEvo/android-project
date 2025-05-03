d4284309114aed0b77cf4918959cac0889a71376
Duc Phu <vandungqc03@gmail.com>
2025-05-03 11:17:02 +0700

    app: add Login/Register workflow with database
    Add the backend logic which store the user password and email
    to database when they sign up and sign in.

--- app/src/main/java/com/example/android_project/loginUI/RegisterActivity.java
+++ app/src/main/java/com/example/android_project/loginUI/RegisterActivity.java
@@ -2,22 +2,31 @@ package com.example.android_project.loginUI;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.example.android_project.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
     EditText EmailET, PasswordET;
+    private FirebaseAuth mAuth;
+    private String TAG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +38,10 @@ public class RegisterActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        TAG = "RegisterActivity";
 
         EmailET = findViewById(R.id.EmailET);
         PasswordET = findViewById(R.id.PasswordET);
@@ -53,8 +66,22 @@ public class RegisterActivity extends AppCompatActivity {
 
                 //Get email and password from EditText fields
                 //and verify with account stored in Database
-                //... Implement Firebase here ...
-
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Log.d(TAG, "createUserWithEmail:success");
+                                    // Sign up success, jump to Login
+                                    login();
+                                } else {
+                                    // If sign up fails, display a message to the user.
+                                    Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                    Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
             }
         });
     }
